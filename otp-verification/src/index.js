f21283a14ed0356082002bdff04d51b290f8cf24
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-07-15 22:49:17 +0530

    Otp verification Setup Done

---
+++ otp-verification/src/index.js
@@ -0,0 +1,56 @@
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+import Redis from "ioredis"
+
+
+const app = express();
+const PORT = process.env.PORT ||  4003
+app.use(express.json()) 
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+function setKey(phoneNumber){
+    return `otp:${phoneNumber}`
+}
+
+function generateOTP(){
+    return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
+app.post("/otp", async(req,res)=>{
+    const {phoneNumber} = req.body     
+    const phoneRegex = /^[6-9]\d{9}$/
+    if (! phoneRegex.test(phoneNumber)) return res.json({message : "Phone Number is not valid"});
+    const otp = generateOTP()
+    await redis.set(setKey(phoneNumber), otp, "EX", 30)
+    return res.json({success : true , otp })  
+    
+})
+
+app.post("/otp/verify", async(req,res)=>{
+    const {phoneNumber,otp} = req.body
+    const storedOTP = await redis.get(setKey(phoneNumber))
+    if(!storedOTP) return res.json({success : false , message : "OTP expired or phone number not found"}).redirect("/otp");
+    if(storedOTP !== otp){
+        const attempts = await redis.incr(`attempts:${phoneNumber}`)
+        if(attempts === 1 || attempts === 2){
+            return res.json({success : false , message :`Invalid OTP .... You have ${3-attempts} attempts left`})
+        }
+        if(attempts >= 3){
+            await redis.del(setKey(phoneNumber))
+            return res.json({success : false , message : "Max Attempts Done ... Create a new OTP"}).redirect("/otp");
+
+        }
+    }
+    
+    return res.json({success : true , message : "OTP Verification Done"});
+    
+})
+
+app.get("/:phoneNumber/ttl", async(req,res) =>{
+    const {phoneNumber} = req.params
+    const ttl = await redis.ttl(setKey(phoneNumber));
+    return res.json({ttl});
+})
+
+app.listen(PORT , () => console.log("Server Running..."));

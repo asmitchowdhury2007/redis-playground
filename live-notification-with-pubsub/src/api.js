f06517dc24aaf595d41d478f147c5dcf60c3e486
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-07-21 23:14:31 +0530

    Refractor Notification Handling

--- live-notification-with-pubsub/src/api.js
+++ live-notification-with-pubsub/src/api.js
@@ -10,8 +10,9 @@ app.use(express.json())
 const publisher = new Redis(process.env.REDIS_URL || "redis://localhost:6379" )
 
 app.post("/notify", async(req,res)=>{
-    const notification = req.body
-    if(!notification) return res.json({success:false , message: "No data given"})
+    const {title,message}= req.body
+    if(!title || !message) return res.json({success:false , message: "No data given"})
+    const notification = {title, message}
     await publisher.publish("notification", JSON.stringify(notification))
     return res.json({success : true, message : "Notification Published"})
 
